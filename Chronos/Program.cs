ac6d5b417e2f8bb0cf73484515e27fafb4df2cd9
SimonXPTO <simaodurao@gmail.com>
2025-03-26 14:22:05 +0000

    Added Chronos to the sln

---
+++ Chronos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+
+            crono1.Start();
+
+            Thread.Sleep(500);
+
+            crono2.Start();
+
+            Thread.Sleep(250);
+
+            crono1.Stop();
+            crono2.Stop();
+
+            Console.WriteLine($"Crono1: {crono1.Elapsed.TotalSeconds:f4}");
+            Console.WriteLine($"Crono2: {crono2.Elapsed.TotalSeconds:f4}");
+
+
+        }
+    }
+}
