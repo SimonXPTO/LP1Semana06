0ca21f6f2c443dd7d9c3bde83bf89f3d991de547
SimonXPTO <simaodurao@gmail.com>
2025-03-27 23:06:02 +0000

    Added RandDice to the solution

---
+++ RandDice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random r1 = new Random(int.Parse(args[1]));
+            int soma=0;
+
+            for (int i = 0; i < int.Parse(args[0]); i++)
+                soma+=r1.Next(1, 7);
+
+            Console.WriteLine(soma);
+        }
+    }
+}
