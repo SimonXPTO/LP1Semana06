3b242847d7a6719c18993e2a94af58f9292ff1d8
SimonXPTO <simaodurao@gmail.com>
2025-03-27 23:34:51 +0000

    Added to Main

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,21 @@ namespace MyGame
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+       
+
+            Enemy[] inimigos = new Enemy[int.Parse(args[0])];
+
+            for (int i = 0; i < int.Parse(args[0]); i++)
+            {
+                Console.Write($"Nome do inimigo {i + 1}: ");
+                string name = Console.ReadLine();
+                inimigos[i] = new Enemy(name);
+            }
+
+            foreach (var inimigo in inimigos)
+            {
+                Console.WriteLine($"{inimigo.GetName()} {inimigo.GetHealth()} {inimigo.GetShield()}");
+            }
         }
     }
 }
