e7f17d3582837552e97dc4269a43ebe0329a7c7f
SimonXPTO <simaodurao@gmail.com>
2025-03-27 23:16:00 +0000

    Added Constructor to Enemy

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -2,6 +2,14 @@ namespace MyGame
 {
     public class Enemy
     {
-      
+        private string name;
+        private float health;
+        private float shield;
+        
+        public Enemy(string name)
+        {
+            this.name = name; 
+            health = 100;
+        }
     }
 }
