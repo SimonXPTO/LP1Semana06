bc5fd667498efa98530ce479b104fa916271c568
SimonXPTO <simaodurao@gmail.com>
2025-03-27 23:11:30 +0000

    Added Enemy and MyGame

---
+++ MyGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyGame
+{
+    public class Enemy
+    {
+    }
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
